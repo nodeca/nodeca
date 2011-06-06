21d7406139c449d16e57979b3ac5fbb4f035aba0
Aleksey V Zapparov <ixti@member.fsf.org>
2011-06-06 22:51:10 +0200

    Fixed docs generator

--- docs/dev-manuals/scripts/support/generate-docs.rb
+++ docs/dev-manuals/scripts/support/generate-docs.rb
@@ -53,7 +53,7 @@ module PDocHelper
       :src_code_href => proc { |obj|
         "https://github.com/#{GITHUB_NAME}/blob/#{current_head}/#{obj.file}#L#{obj.line_number}"
       },
-      :pretty_urls => true,
+      :pretty_urls => false,
       :bust_cache => true,
       :name => PROJECT_NAME,
       :version => version
