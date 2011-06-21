ec04b9c0e9719db26efb2e0b3ef5e27c54ff671d
Aleksey V Zapparov <ixti@member.fsf.org>
2011-06-21 12:22:48 +0200

    Attempted to sync with Application class

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var path = require('path'),
     nodeca = require('nodeca-lib'),
     Promise = nodeca.Promise,
     $$ = nodeca.Utilities,
-    app = new nodeca.Application(__dirname, ['application.yml']);
+    configFile = path.join(app.dirname, 'config', 'application.yml');
 
 
 // Outputs error to stderr and terminates process with given code
@@ -27,93 +27,73 @@ var ERR_START   = 129;
 
 
 // initialize application
-app.init(function (err, app, config) {
-  if (err) {
-    halt(err, ERR_INIT);
-  }
-
-  try {
-    // create server and run it
-    var server = express.createServer();
-
-    // set view engine and some default options
-    server.set('view engine', 'jade');
-    server.set('view options', {layout: 'layouts/default'});
-
-    // set request handlers chain
-    server.use(express.static(path.join(__dirname, 'public')));
-    server.use(express.bodyParser());
-    server.use(express.methodOverride());
-    server.use(express.cookieParser());
-
-    server.helper('__', function __(str, params, context) {
-      // basically we can get req and res from `this` afaik
-      return str;
+try {
+  var app = new nodeca.Application(__dirname, function bootstrapper() {
+    this.addHook('schemas-loaded', function () {
+      this.log("Schemas were loaded! Cool!");
     });
+  }).init($$.readYamlSync(configFile, true));
 
-    server.use(server.router);
 
-    // last handler starts new cycle with error
-    server.use(function RouteNotFound(req, res, next) {
-      var err  = new Error('Not Found');
-      err.code = 404;
-      return next(err);
-    });
+  // create server and run it
+  var server = express.createServer();
 
-    // register rerror handler should be configured
-    server.error((function (dispatcher) {
-      var controller = 'errors';
-      var action     = 'error';
+  // set view engine and some default options
+  server.set('view engine', 'jade');
+  server.set('view options', {layout: 'layouts/default'});
 
-      if (!dispatcher.isDispatchable({"controller": controller, "action": action})) {
-        return function (err, req, res, next) {
-          res.send(err);
-        }
-      }
+  // set request handlers chain
+  server.use(express.static(path.join(__dirname, 'public')));
+  server.use(express.bodyParser());
+  server.use(express.methodOverride());
+  server.use(express.cookieParser());
 
-      return function(err, req, res, next) {
-        logger.error(err, req);
-        logger.debug(err.stack);
-
-        req.originalController  = req.controller;
-        req.originalAction      = req.action;
-        req.controller          = controller;
-        req.action              = action;
-        req.error               = err;
-
-        dispatcher.dispatch(req, res, next);
-      }
-    })(app.dispatcher));
-  } catch (err) {
-    halt(err, ERR_START);
-  }
-
-  // init sub-apps
-  $$.iterate(config.modules || {}, function next(mod_name, mod_config, next) {
-    app.mount(mod_name, mod_config, function (err) {
-      if (err) {
-        halt(err, ERR_INIT);
-      }
-      next();
-    });
-  }, function final(err) {
-    if (err) {
-      halt(err, ERR_INIT);
-    }
-
-    // inject routes
-    app.router.inject(server);
-
-    // register heplers
-    server.helpers({
-      config: function (section) { return config[section]; }
-    });
+  server.helper('__', function __(str, params, context) {
+    // basically we can get req and res from `this` afaik
+    return str;
   });
 
-  // start server
-  var listen = $$.deepMerge({port: 8000}, config.listen);
-  server.listen(listen.port, listen.host);
-});
+  server.use(server.router);
+
+  // last handler starts new cycle with error
+  server.use(function RouteNotFound(req, res, next) {
+    var err  = new Error('Not Found');
+    err.code = 404;
+    return next(err);
+  });
+
+  // register rerror handler should be configured
+  server.error((function (dispatcher) {
+    var controller = 'errors';
+    var action     = 'error';
+
+    if (!dispatcher.isDispatchable({"controller": controller, "action": action})) {
+      return function (err, req, res, next) {
+        res.send(err);
+      }
+    }
+
+    return function(err, req, res, next) {
+      logger.error(err, req);
+      logger.debug(err.stack);
+
+      req.originalController  = req.controller;
+      req.originalAction      = req.action;
+      req.controller          = controller;
+      req.action              = action;
+      req.error               = err;
+
+      dispatcher.dispatch(req, res, next);
+    }
+  })(app.dispatcher));
+} catch (err) {
+  halt(err, ERR_START);
+}
+
+
+// start server
+var listen = $$.deepMerge({port: 8000}, app.config.listen);
+server.listen(listen.port, listen.host);
 
 
 ////////////////////////////////////////////////////////////////////////////////
