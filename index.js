54e2b89b71633904e500116f7b07275c1f11a1a9
Aleksey V Zapparov <ixti@member.fsf.org>
2011-06-23 23:35:22 +0200

    Updated starter with latest lib trends

--- index.js
+++ index.js
@@ -26,9 +26,12 @@ var ERR_INIT    = 128;
 var ERR_START   = 129;
 
 
+var app; // Application instance
+
+
 // initialize application
 try {
-  var app = new nodeca.Application(__dirname, function bootstrapper() {
+  app = new nodeca.Application(__dirname, function bootstrapper() {
     this.addHook('schemas-loaded', function () {
       this.log("Schemas were loaded! Cool!");
     });
@@ -40,62 +43,57 @@ try {
 }
 
 
-// create server and run it
-var server = express.createServer();
+// application is ready to start
+app.ready(function () {
+  // create server and run it
+  var server = express.createServer();
 
-// set view engine and some default options
-server.set('view engine', 'jade');
-server.set('view options', {layout: 'layouts/default'});
+  // set view engine and some default options
+  server.set('view engine', 'jade');
+  server.set('view options', {layout: 'layouts/default'});
 
-// set request handlers chain
-server.use(express.static(path.join(__dirname, 'public')));
-server.use(express.bodyParser());
-server.use(express.methodOverride());
-server.use(express.cookieParser());
+  // set request handlers chain
+  server.use(express.static(path.join(__dirname, 'public')));
+  server.use(express.bodyParser());
+  server.use(express.methodOverride());
+  server.use(express.cookieParser());
 
-server.helper('__', function __(str, params, context) {
-  // basically we can get req and res from `this` afaik
-  return str;
-});
+  server.helper('__', function __(str, params, context) {
+    // basically we can get req and res from `this` afaik
+    return str;
+  });
 
-server.use(server.router);
+  server.use(server.router);
 
-// last handler starts new cycle with error
-server.use(function RouteNotFound(req, res, next) {
-  var err  = new Error('Not Found');
-  err.code = 404;
-  return next(err);
-});
+  // last handler starts new cycle with error
+  server.use(function RouteNotFound(req, res, next) {
+    var err  = new Error('Not Found');
+    err.code = 404;
+    return next(err);
+  });
 
-// register rerror handler should be configured
-server.error((function (dispatcher) {
-  var controller = 'errors';
-  var action     = 'error';
-
-  if (!dispatcher.isDispatchable({"controller": controller, "action": action})) {
-    return function (err, req, res, next) {
-      res.send(err);
-    }
-  }
-
-  return function(err, req, res, next) {
-    logger.error(err, req);
-    logger.debug(err.stack);
+  // register rerror handler should be configured
+  server.error(function(err, req, res, next) {
+    app.log.error(err, req).error(err.stack);
 
     req.originalController  = req.controller;
     req.originalAction      = req.action;
-    req.controller          = controller;
-    req.action              = action;
+    req.controller          = 'errors';
+    req.action              = 'error';
     req.error               = err;
 
-    dispatcher.dispatch(req, res, next);
-  }
-})(app.dispatcher));
+    app.dispatcher.dispatch(req, res, next);
+  });
 
 
-// start server
-var listen = $$.deepMerge({port: 8000}, app.config.listen);
-server.listen(listen.port, listen.host);
+  // start server
+  var listen = $$.merge({port: 8000}, app.config.listen);
+  server.listen(listen.port, listen.host);
+});
+
+
+// spmething went wrong during app initializtion
+app.fail(halt);
 
 
 ////////////////////////////////////////////////////////////////////////////////
