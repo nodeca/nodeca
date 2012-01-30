e6dd264edfad01e4f74b413f437e13109c64a5c0
Aleksey V Zapparov <ixti@member.fsf.org>
2012-01-30 13:39:02 +0100

    Prepare application entry point

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 'use strict';
 
-var NodecaLib = require('nodeca-lib');
-
-// Create application instance
-var app = new NodecaLib.Application({name: 'nodeca', root: __dirname});
-
-// run application
-app.run();
+// Create and run app
+require('nlib').Application.create({
+  name: 'nodeca',
+  root: __dirname
+}).run();
