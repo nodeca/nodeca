6f446784d5f5088bddf6321be44ba8d91b79103c
Aleksey V Zapparov <ixti@member.fsf.org>
2013-01-04 19:31:07 +0100

    WIP: refactor stack

---
+++ cli/server.js
@@ -0,0 +1,53 @@
+"use strict";
+
+
+/*global N*/
+
+
+// 3rd-party
+var async = require('async');
+
+
+module.exports.parserParameters = {
+  version:      N.runtime.version,
+  addHelp:      true,
+  help:         'start nodeca server',
+  description:  'Start nodeca server'
+};
+
+
+module.exports.commandLineArguments = [
+  {
+    args: ['--test'],
+    options: {
+      help:   'Start server an terminates immediately, ' +
+              'with code 0 on init success.',
+      action: 'storeTrue'
+    }
+  }
+];
+
+
+module.exports.run = function (args, callback) {
+
+  async.series([
+    function (next) { N.logger.debug('Init app...'); next(); },
+    require('../lib/init/app'),
+
+    function (next) { N.logger.debug('Init server...'); next(); },
+    require('../lib/init/server')
+  ], function (err) {
+    if (err) {
+      callback(err);
+      return;
+    }
+
+    // for `--test` just exit on success
+    if (args.test) {
+      process.stdout.write('Server exec test OK\n');
+      process.exit(0);
+    }
+
+    callback();
+  });
+};
--- nodeca.js
+++ nodeca.js
@@ -1,80 +1,16 @@
 #!/usr/bin/env node
-'use strict';
 
 
-/*global nodeca*/
+"use strict";
 
 
-//
-// Create app
-//
-
-var app = require('nlib').Application.create({
-  name: 'nodeca',
-  root: __dirname
-});
+exports.root = __dirname;
+exports.init = function () {
+  // Executed once all application were loaded.
+  // Used to "attach" hooks for models etc.
+};
 
 
-//
-// Preset application version
-//
-
-
-nodeca.runtime.version = require('./package.json').version;
-
-
-//
-// Catch unexpected exceptions
-//
-
-
-process.on('uncaughtException', function (err) {
-  var msg, stack;
-
-  try {
-    msg   = 'UNCAUGHT EXCEPTION !!! ' + String(err);
-    stack = err.stack || '';
-
-    if (err.original) {
-      msg += '\n' + String(err.original);
-      stack = err.original.stack || stack;
-    }
-
-    msg += '\n' + stack.split('\n').slice(1).join('\n');
-
-    nodeca.logger.fatal(msg);
-  } catch (loggerError) {
-    // THIS SHOULD NEVER-EVER-EVER HAPPEN -- THIS IS A WORST CASE SCENARIO
-    // USAGE: ./nodeca.js 2>/var/log/nodeca-cf.log
-    process.stderr.write('\nLogger failed write: ' + loggerError.stack);
-    process.stderr.write('\nOriginal error happened: ' + msg);
-  }
-
-  if ('test' === nodeca.runtime.env) {
-    process.exit(1);
-  }
-});
-
-
-//
-// Handle SIGnals
-//
-
-
-function shutdown_gracefully() {
-  nodeca.logger.info('Shutting down...');
-  process.exit(0);
+if (!module.parent) {
+  require('./lib/runner').bootstrap(exports);
 }
-
-
-// shutdown gracefully on SIGTERM :
-process.on('SIGTERM', shutdown_gracefully);
-process.on('SIGINT',  shutdown_gracefully);
-
-
-//
-// Run application
-//
-
-
-app.run();
