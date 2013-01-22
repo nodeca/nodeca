7155a7dd3c67d11c3284f829e448ed8074425363
Vitaly Puzrin <vitaly@rcdesign.ru>
2013-01-22 10:27:36 +0400

    increased trace size & updated comments

--- nodeca.js
+++ nodeca.js
@@ -3,13 +3,13 @@
 
 "use strict";
 
+Error.stackTraceLimit = 30;
 
 exports.root = __dirname;
-exports.init = function () {
-  // Executed once all application were loaded.
-  // Used to "attach" hooks for models etc.
-};
 
+// Executed once all application were loaded.
+// Used to "attach" hooks for models etc.
+/* exports.init = function (N) {}; */
 
 if (!module.parent) {
   require('nodeca.core/lib/system/runner').bootstrap(exports);
