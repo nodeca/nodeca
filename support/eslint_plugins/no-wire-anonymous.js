79bcc78868dfd66464406372044bc3857091a4d4
Kirill Efimov <kirill89@gmail.com>
2014-06-25 12:17:38 +0400

    Added rule to check anonymous functions as 'N.wire...' arguments

---
+++ support/eslint_plugins/no-wire-anonymous.js
@@ -0,0 +1,39 @@
+/**
+ * @fileoverview Rule to flag use of anonymous functions
+ * as callback of N.wire.on, N.wire.before, N.wire.after
+ *
+ * @author Nodeca Team <https://github.com/nodeca>
+ */
+
+'use strict';
+
+//------------------------------------------------------------------------------
+// Rule Definition
+//------------------------------------------------------------------------------
+
+module.exports = function (context) {
+
+  return {
+    'CallExpression': function (node) {
+      if (node.callee && node.callee.property) {
+        var callee = node.callee;
+
+        if ([ 'on', 'before', 'after' ].indexOf(callee.property.name) !== -1) {
+          var calleeObj = node.callee.object;
+
+          if (calleeObj && calleeObj.property && calleeObj.property.name === 'wire') {
+
+            if (calleeObj.object && calleeObj.object.name === 'N') {
+              node.arguments.forEach(function (arg) {
+                // FunctionExpression with name will have 'id' property
+                if (arg.type === 'FunctionExpression' && !arg.id) {
+                  context.report(node, 'Don\'t use anonymous function here.');
+                }
+              });
+            }
+          }
+        }
+      }
+    }
+  };
+};
