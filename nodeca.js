0a4c5bbfd0fb0c252dc3196935ddc56772492204
Vitaly Puzrin <vitaly@rcdesign.ru>
2015-12-25 19:59:08 +0300

    removed libvips kludge (not needed with sharp 0.12.1)

--- nodeca.js
+++ nodeca.js
@@ -11,10 +11,6 @@ exports.root = __dirname;
 // Used to "attach" hooks for models etc.
 /* exports.init = function (N) {}; */
 
-// disable orc at runtime to avoid segfaults,
-// see https://github.com/lovell/sharp/issues/172#issuecomment-162800414
-process.env.VIPS_NOVECTOR = 1;
-
 if (!module.parent) {
   require('nodeca.core/lib/system/runner').bootstrap(exports);
 }
