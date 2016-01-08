97d5086afa735c58911b7695b71aa90fedad2772
Vitaly Puzrin <vitaly@rcdesign.ru>
2016-01-09 00:55:43 +0300

    Simplify start

--- nodeca.js
+++ nodeca.js
@@ -1,16 +1,11 @@
 #!/usr/bin/env node
-
-
 'use strict';
+
 
 Error.stackTraceLimit = 30;
 
 exports.root = __dirname;
 
-// Executed once all application were loaded.
-// Used to "attach" hooks for models etc.
-/* exports.init = function (N) {}; */
-
 if (!module.parent) {
-  require('nodeca.core/lib/system/runner').bootstrap(exports);
+  require('nodeca.core').run(exports);
 }
